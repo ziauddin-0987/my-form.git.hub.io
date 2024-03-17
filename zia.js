3f88d6ba8984bbaa9a40cb4e1154451f0e250028
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-17 17:36:37 +0500

    there are my three files for page in form

---
+++ zia.js
@@ -0,0 +1,49 @@
+// // const emai = prompt("print your gmail.com");
+// // const pass = prompt("print your password");
+// function fromm() {
+//   const emaill = document.getElementById("passw").value;
+//   const pass = document.getElementById("emailo").vlaue;
+
+//   if (
+//     (emaill == "zia@gmail.com" && pass == "khan") ||
+//     (emaill == "irfan@gmail.com" && pass == "irfan")
+//     // (emaill == "zeshan@gmail.com" && pass == "zeshan") ||
+//     // (emaill == "naeem@gmail.com" && pass == "naeem") ||
+//     // (emaill == "ubaid@gmail.com" && pass == "ubaid") ||
+//     // (emaill == "izzat@gmail.com" && pass == "izzat") ||
+//     // (emaill == "hikmat@gmail.com" && pass == "hikmat") ||
+//     // (emaill == "naveed@gmail.com" && pass == "naveed") ||
+//     // (emaill == "qayyum@gmail.com" && pass == "qayyum")
+//   ) {
+//     console.log("correact your passworw and email ");
+//   } else {
+//     console.log("not valiide your password and email");
+//   }
+// }
+function fromm() {
+  //   console.clear();
+  const emaill = document.getElementById("emailo").value;
+  const passwordd = document.getElementById("passw").value;
+  if (
+    (emaill == "zia@gmail.com" && passwordd == "ziauddin") ||
+    (emaill == "zubar@gmail.com" && passwordd == "zubar") ||
+    (emaill == "umair@gmail.com" && passwordd == "umair") ||
+    (emaill == "irfan@gmail.com" && passwordd == "irfan")
+  ) {
+    console.log("corract your email and password");
+  } else {
+    console.log("your are not allow password and gmail");
+  }
+}
+
+// document.write("irfan@gmai.com "+" irfan"
+//  zeshan@gmai.com+"zeshan"
+// naeem@gmai.com&&pass naeem
+// ubaid@gmai.com&&pass =ubaid
+// izzat@gmai.com"&&pass =izzat
+// hikmat@gmai.com"&&pass "hikmat
+// naveed@gmai.com pass "naveed
+// quayyam@gmai.com"&&pass="quayyam
+// ")
+
+// (emai=="irfan@gmai.com"&&pass =="irfan")
